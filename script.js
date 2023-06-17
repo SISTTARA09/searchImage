18e77cba858691aca8651422213cfdcd0b665a75
SISTTARA09 <yassinesisttara06@gmial.com>
2023-06-17 10:58:34 +0100

    Replace The Append Methode

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const cl = console.log;
 let accesKey = "aA3w5gQhatBLRfXP5sQGBPeyNxhW3ayTOlfb3fgfQxE";
 let submit = document.getElementById("submit");
 let searchInp = document.getElementById("searchInp");
-let searchForm = document.getElementById('searchForm')
+let searchForm = document.getElementById("searchForm");
 let showBox = document.querySelector(".show-box");
 let showMoreBtn = document.getElementById("showMore");
 let num = Math.random().toFixed(1) * 10;
 searchForm.addEventListener("submit", async (ev) => {
-  ev.preventDefault()
+	ev.preventDefault();
 	showBox.textContent = "";
 	// WORK WITH API
 	let mainData = await fetch(
@@ -21,19 +21,12 @@ searchForm.addEventListener("submit", async (ev) => {
 		for (let i in data.results) {
 			let url = data.results[i].links.download;
 			let imgPage = data.results[i].links.html;
-			let box = document.createElement("div");
-			box.setAttribute("class", "box");
-			let anchor = document.createElement("a");
-			anchor.setAttribute("href", imgPage);
-			anchor.target = "_blank";
-			let img = document.createElement("img");
-			img.setAttribute("src", url);
-			let p = document.createElement("p");
-			p.setAttribute("class", "description");
-			p.textContent = data.results[i].alt_description;
-			anchor.append(img, p);
-			box.append(anchor);
-			showBox.append(box);
+			showBox.innerHTML += `<div class="box">
+			<a href="${imgPage}" target="_blank">
+			<img src="${url}">
+			<p class="description">${data.results[i].alt_description}</p>
+			</a>
+			</div>` 
 		}
 	} catch (reason) {
 		cl(reason);
@@ -53,22 +46,16 @@ searchForm.addEventListener("submit", async (ev) => {
 			for (let i in data.results) {
 				let url = data.results[i].links.download;
 				let imgPage = data.results[i].links.html;
-				let box = document.createElement("div");
-				box.setAttribute("class", "box");
-				let anchor = document.createElement("a");
-				anchor.setAttribute("href", imgPage);
-				anchor.target = "_blank";
-				let img = document.createElement("img");
-				img.setAttribute("src", url);
-				let p = document.createElement("p");
-				p.setAttribute("class", "description");
-				p.textContent = data.results[i].alt_description;
-				anchor.append(img, p);
-				box.append(anchor);
-				showBox.append(box);
+				showBox.innerHTML += `<div class="box">
+				<a href="${imgPage}" target="_blank">
+				<img src="${url}">
+				<p class="description">${data.results[i].alt_description}</p>
+				</a>
+				</div>`
 			}
 		} catch (re) {
 			cl(re);
 		}
 	};
 });
+('<div class="box"><a href="https://unsplash.com/photos/boEl1XGLZ10" target="_blank"><img src="https://unsplash.com/photos/boEl1XGLZ10/download?ixid=M3w0NjI2MzZ8MHwxfHNlYXJjaHwzMXx8ZnxlbnwwfHx8fDE2ODY5OTI3MDh8MA"><p class="description">a green sports car parked in front of a building</p></a></div>');
